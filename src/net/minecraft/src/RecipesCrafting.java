09360d7e7f34028339b46638e2fd0719dfcdbab6
rahulinmaksym <rahulinmaksym@gmail.com>
2025-05-09 02:05:39 +0300

    Recipe for BlockOpenHearthFurnace, TODO.txt, terrain.png, items.png, en_US.lang

--- src/net/minecraft/src/RecipesCrafting.java
+++ src/net/minecraft/src/RecipesCrafting.java
@@ -7,7 +7,7 @@ public class RecipesCrafting {
 		var1.addRecipe(new ItemStack(Block.workbench), new Object[]{"##", "##", Character.valueOf('#'), Block.planks});
 		var1.addRecipe(new ItemStack(Block.sandStone), new Object[]{"##", "##", Character.valueOf('#'), Block.sand});
 		var1.addRecipe(new ItemStack(Block.openHearthFurnaceIdle, 1), new Object[]{
-				"# #", "###", "###",
+				"# #", "#B#", "#N#",
 				Character.valueOf('#'), Block.cobblestone,
 				Character.valueOf('B'), Item.bucketEmpty,
 				Character.valueOf('N'), Block.netherrack
